3c8ae9142bf6a5af8fccd539b154624a5467845d
lensonfernandes <lenson.fernand@gmail.com>
2023-01-13 12:28:15 +0530

    Logo in Header added

---
+++ houseofpepperv4/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import './index.css';
+import './App.css';
+import { Header } from './components';
+function App() {
+  return (
+    <div className="App w-screen h-auto flex flex-col">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ houseofpepperv4/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from '../img/img/logo.png'
+
+const Header = () => {
+  return (
+    <div className='fixed z-50 w-screen  p-6 px-16 '>
+        {/* Desktop */}
+        <div className='hidden md:flex w-full h-full '>
+              <div className="flex items-center gap-2">
+                  <img src={Logo} className="w-10 object-cover" alt="logo"></img>
+              </div>
+        </div>
+{/* mobile */}
+        <div className='flex md:hidden w-full h-full '>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
