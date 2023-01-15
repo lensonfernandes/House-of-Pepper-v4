848310acdb28fb3860ffc0958746d6318536236d
lensonfernandes <lenson.fernand@gmail.com>
2023-01-15 15:33:00 +0530

    Added New Item Route Link to Admin

--- houseofpepperv4/src/components/Header.jsx
+++ houseofpepperv4/src/components/Header.jsx
@@ -3,34 +3,34 @@ import Logo from "../img/img/logo.png";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import Avatar from "../img/img/avatar.png";
 import { motion } from "framer-motion";
-import {Link} from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
 import { useStateValue } from "../Context/StateProvider";
 import { actionType } from "../Context/reducer";
-
-
-
-
+import { MdAdd, MdLogout } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
 
-const firebaseAuth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-const [{user}, dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue();
 
   let login = async () => {
+    if (!user) {
+      let {
+        user: { refreshToken, providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch({
+        type: actionType.SET_USER,
+        user: providerData[0],
+      });
+      // console.log(response)
 
-    let {user : {refreshToken, providerData}} = await  signInWithPopup(firebaseAuth, provider);
-    dispatch({
-      type: actionType.SET_USER,
-      user: providerData[0]
-    })
-    // console.log(response)
-
-    localStorage.setItem('user', JSON.stringify(providerData[0]))
-  }
+      localStorage.setItem("user", JSON.stringify(providerData[0]));
+    }
+  };
 
   return (
     <div className="fixed z-50 w-screen  p-6 px-16 ">
@@ -52,14 +52,25 @@ const [{user}, dispatch] = useStateValue()
           2
         </span>
         <div className="flex items-center ml-3">
-         
           <motion.img
-          whileTap={{scale:0.6}}
+            whileTap={{ scale: 0.6 }}
             src={user ? user.photoURL : Avatar}
             className="w-10 h-10 object-cover cursor-pointer rounded-full "
             alt="avatar"
             onClick={login}
           />
+        </div>
+        <div className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute px-4 py-4 top-12 right-0">
+          {user && user.email === "lenson.fernand@gmail.com" && (
+            <Link to='/createItem'>
+              <p className=" py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in">
+                New Item <MdAdd />
+              </p>
+            </Link>
+          )}
+          <p className=" py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out">
+            Logout <MdLogout />{" "}
+          </p>
         </div>
       </div>
       {/* mobile */}
